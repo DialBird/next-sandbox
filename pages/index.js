bafe809345a6655adebcde30c40171e6a9841c6f
keisuke <rememberthatk@icloud.com>
2020-04-14 09:22:08 +0900

    [master] add link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+
 const Index = () => {
   return (
     <div>
+      <Link href='/about'>
+        <a title="About Page">About Page</a>
+      </Link>
       <p>Hello Next.js</p>
     </div>
   );
