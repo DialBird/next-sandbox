dd7d2c61e50c55eaa8d7abe424ff92a345f003ac
keisuke <rememberthatk@icloud.com>
2020-04-14 09:25:10 +0900

    [master] add Header

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Link from 'next/link';
+import Header from '../components/Header';
 
 const Index = () => {
   return (
     <div>
-      <Link href='/about'>
-        <a title="About Page">About Page</a>
-      </Link>
+      <Header/>
       <p>Hello Next.js</p>
     </div>
   );
