c726d19a5aeba9a50e6179c10ac251a99877efe8
keisuke <rememberthatk@icloud.com>
2020-04-14 09:27:37 +0900

    [master] add Layout

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
+import Layout from '../components/MyLayout';
+
 const About = () => {
   return (
-    <div>
+    <Layout>
       <p>This is the about page</p>
-    </div>
+    </Layout>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Header from '../components/Header';
+import Layout from '../components/MyLayout';
 
 const Index = () => {
   return (
-    <div>
-      <Header/>
+    <Layout>
       <p>Hello Next.js</p>
-    </div>
+    </Layout>
   );
 }
 
