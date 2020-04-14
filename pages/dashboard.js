0cc06582b54a12caed79b586650da1ad8fb2e455
keisuke <rememberthatk@icloud.com>
2020-04-15 07:58:10 +0900

    [master] add dashboard

---
+++ pages/dashboard.js
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import axios from 'axios';
+
+const PostLink = props => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
+    </Link>
+  </li>
+);
+
+const Dashboard = props => {
+  return (
+    <React.Fragment>
+      <h1>Batman TV Shows</h1>
+      <ul>
+        {props.shows.map(show => (
+          <li key={show.id}>
+            <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
+        <PostLink id="hello-nextjs" />
+        <PostLink id="learn-nextjs" />
+        <PostLink id="deploy-nextjs" />
+      </ul>
+    </React.Fragment>
+  );
+}
+
+Dashboard.getInitialProps = async () => {
+  const res = await axios.get('https://api.tvmaze.com/search/shows?q=batman')
+  return {
+    shows: res.data.map(entry => entry.show)
+  };
+}
+
+export default Dashboard;
