52ad10e4d9c3c465f2d4bf950ff0faf3681fa0eb
keisuke <rememberthatk@icloud.com>
2020-04-14 10:22:09 +0900

    [master] add axios render

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
+import axios from 'axios';
 
 const PostLink = props => (
   <li>
@@ -9,17 +10,31 @@ const PostLink = props => (
   </li>
 );
 
-const Index = () => {
+const Index = props => {
   return (
     <Layout>
-      <h1>My Blog</h1>
+      <h1>Batman TV Shows</h1>
       <ul>
+        {props.shows.map(show => (
+          <li key={show.id}>
+            <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
         <PostLink id="hello-nextjs" />
         <PostLink id="learn-nextjs" />
         <PostLink id="deploy-nextjs" />
       </ul>
     </Layout>
   );
+}
+
+Index.getInitialProps = async () => {
+  const res = await axios.get('https://api.tvmaze.com/search/shows?q=batman')
+  return {
+    shows: res.data.map(entry => entry.show)
+  };
 }
 
 export default Index;
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,15 +1,23 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout';
+import axios from 'axios';
 
-const Post = () => {
-  const router = useRouter();
+const Post = props => {
+  const { show }= props;
 
   return (
     <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
+      <h1>{show.name}</h1>
+      <p>{show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+      {props.show.image ? <img src={props.show.image.medium} /> : null}
     </Layout>
   );
 };
+
+Post.getInitialProps = async context => {
+  const { id } = context.query;
+  const res = await axios.get(`https://api.tvmaze.com/shows/${id}`);
+  const show = res.data;
+  return { show };
+}
 
 export default Post;
