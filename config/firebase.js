a1c94654db9c1d3b9469091a2c9e6b7588a828b8
keisuke <rememberthatk@icloud.com>
2020-04-14 16:20:08 +0900

    [master] add config/firebase

---
+++ config/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHiTShJd-oDreoWi6xCYnt07tjZYh-dvY",
+  authDomain: "sample-f5a0c.firebaseapp.com",
+  databaseURL: "https://sample-f5a0c.firebaseio.com",
+  projectId: "sample-f5a0c",
+  storageBucket: "sample-f5a0c.appspot.com",
+  messagingSenderId: "63915581384",
+  appId: "1:63915581384:web:58b8645f4ca8580d"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore();
+
+export default firebase;
