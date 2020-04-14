eb0846fa876a5a830d08d8b1e4800f16b48315e2
keisuke <rememberthatk@icloud.com>
2020-04-14 20:05:18 +0900

    [master] add signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,31 @@
+import { useContext } from 'react';
+
+import AuthContext from '../contexts/AuthContext';
 import Layout from '../components/MyLayout';
 
 const Signup = () => {
+  const { signup } = useContext(AuthContext);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { email, password } = e.target.elements;
+    signup(email.value, password.value);
+  }
+
   return (
     <Layout>
       <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email address</label>
+          <input type="email" className="form-control" aria-describedby="emailHelp" name='email' placeholder="Enter email"/>
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" className="form-control" name='password' placeholder="Password"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </Layout>
   );
 }
