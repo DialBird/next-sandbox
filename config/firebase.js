a44451f061d7fc2b64cab6a81bec74a1fa6767a7
keisuke <rememberthatk@icloud.com>
2020-04-15 10:02:43 +0900

    [master] replace firebase config keys

--- config/firebase.js
+++ config/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHiTShJd-oDreoWi6xCYnt07tjZYh-dvY",
-  authDomain: "sample-f5a0c.firebaseapp.com",
-  databaseURL: "https://sample-f5a0c.firebaseio.com",
-  projectId: "sample-f5a0c",
-  storageBucket: "sample-f5a0c.appspot.com",
-  messagingSenderId: "63915581384",
-  appId: "1:63915581384:web:58b8645f4ca8580d"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 if (!firebase.apps.length) {
